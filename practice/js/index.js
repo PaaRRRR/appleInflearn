a6d3e6c56e599fcdbd7329bc47c8c27f1a6dd2e0
Haein Cho <haein@3i.ai>
2020-06-19 08:36:45 +0900

    initial commit

---
+++ practice/js/index.js
@@ -0,0 +1,37 @@
+(function() {
+  let yOffset = 0; // window.pageYOffset 대신 쓸 변수
+  let prevScrollHeight = 0; // 현재 스크롤 위치(yOffset)보다 이전에 위치한 스크롤 섹션들의 스크롤 높이값의 합
+  let currentScene = 0; // 현재 활성화된(눈 앞에 보고있는) 씬(scroll-section)
+  let enterNewScene = false; // 새로운 scene이 시작된 순간 true
+
+  const sceneInfo = [
+    {
+      type: "sticky",
+      heightNum: 5, // 브라우저 높이의 5배로 scrollHeight 세팅
+      scrollHeight: 0,
+      objs: {
+        container: document.querySelector("#scroll-section-0"),
+        messageA: document.querySelector("#scroll-section-0 .main-message.a"),
+        messageB: document.querySelector("#scroll-section-0 .main-message.b"),
+        messageC: document.querySelector("#scroll-section-0 .main-message.c"),
+        messageD: document.querySelector("#scroll-section-0 .main-message.d"),
+        canvas: document.querySelector("#video-canvas-0"),
+        context: document.querySelector("#video-canvas-0").getContext("2d"),
+        videoImages: []
+      },
+      values: {
+        videoImageCount: 300,
+        imageSequence: [0, 299],
+        canvas_opacity: [1, 0, { start: 0.9, end: 1 }],
+        messageA_transform: {
+          opacity_in: [0, 1, { start: 0.1, end: 0.2 }],
+          opacity_out: [1, 0, { start: 0.25, end: 0.3 }],
+          translateY_in: [20, 0, { start: 0.1, end: 0.2 }],
+          translateY_out: [0, -20, { start: 0.25, end: 0.3 }]
+        }
+      }
+    }
+  ];
+})();
+
+function init() {}
